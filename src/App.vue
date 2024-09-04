6feb163805b3103e41c28900c93d7a00597ce61d
nashafy <nashafy@bluemint.ai>
2024-09-04 12:29:05 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<template>
+  <div class="card-container">
+    <h1 style="font-weight: bold">Search</h1>
+    <br />
+    <form @submit.prevent="handleSearch">
+      <input
+        type="text"
+        v-model="searchTerm"
+        placeholder="Search for a word"
+        @input="handleSearch"
+        size="50"
+      />
+      <!-- <button type="submit">Search</button> -->
+      <br />
+      <br />
+      <span v-if="highlightedText"
+        >{{ occurrences }} of {{ highlightedText }} were found</span
+      >
+    </form>
+    <br />
+    <p v-html="highlightText(paragraph, highlightedText)"></p>
+  </div>
+</template>
+
 <script>
 import { ref } from "vue";
 
@@ -7,7 +31,7 @@ export default {
     const highlightedText = ref("");
     const occurrences = ref(0);
     const paragraph =
-      "this is an example paragraph , you can search for a word in this paragraph";
+      "Understanding the difference between grid-template and grid-auto Oct 09, 2018 With all the new properties related to CSS Grid Layout, one of the distinctions that always confused me was the difference between the grid-template-* and grid-auto-* properties. Specifically the difference between grid-template-rows/columns and grid-auto-rows/columns. Although I knew they were to d... Recreating the GitHub Contribution Graph with CSS Grid Lavout ";
     const handleSearch = () => {
       highlightedText.value = searchTerm.value;
     };
@@ -38,18 +62,19 @@ export default {
 };
 </script>
 
-<template>
-  <div>
-    <p v-html="highlightText(paragraph, highlightedText)"></p>
-    <form @submit.prevent="handleSearch">
-      <input type="text" v-model="searchTerm" placeholder="Search for a word" />
-      <div>{{ occurrences }} of {{ highlightedText }} were found</div>
-      <button type="submit">Search</button>
-    </form>
-  </div>
-</template>
-
 <style>
+.card-container {
+  background: #fff; /* White background */
+  border-radius: 8px; /* Rounded corners */
+  padding: 20px; /* Space inside the card */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Subtle shadow */
+  max-width: 600px; /* Maximum width of the card */
+  margin: 20px auto; /* Center the card horizontally with some top/bottom margin */
+}
+
+input {
+  width: calc(100% - 22px); /* Full width minus padding and border */
+}
 div {
   padding: 20px;
   font-family: Arial, sans-serif;
